d5fe01eda16b18815312ee0b42e6e1c7c3e8720b
martinmalinda <malindacz@gmail.com>
2020-05-14 16:36:04 +0200

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  globals: {},
+
+  moduleFileExtensions: ["js", "ts"],
+
+  moduleNameMapper: {},
+};
