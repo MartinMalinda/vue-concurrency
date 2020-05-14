e5732aa9bc20d6fccf9b09a6f0a6fdc050dfeee6
martinmalinda <malindacz@gmail.com>
2020-05-14 16:55:50 +0200

    add tsconfig

--- test-utils/vue-setup.ts
+++ test-utils/vue-setup.ts
@@ -1,6 +1,4 @@
-import Vue from "vue";
+import * as Vue from "vue";
 import CompositionApi from "@vue/composition-api";
 
-console.log({ Vue, CompositionApi });
-
-// Vue.use(CompositionApi);
+Vue.default.use(CompositionApi);
