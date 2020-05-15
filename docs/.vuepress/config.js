a80b94a9a037d77de6f2b3f3a4be0d722b4c3e76
martinmalinda <malindacz@gmail.com>
2020-05-15 13:44:34 +0200

    add docs structure

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+// .vuepress/config.js
+module.exports = {
+  title: "🚦vue-concurrency",
+  description:
+    "A library for encapsulating asynchronous operations and managing concurrency",
+  themeConfig: {
+    repo: "martinmalinda/vue-concurrency",
+    theme: "@vuepress/vue",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/introduction/" },
+    ],
+    sidebar: [
+      {
+        title: "Theory",
+        collapsable: false,
+        children: ["/introduction/", "/benefits/"],
+      },
+      {
+        title: "Practice",
+        collapsable: false,
+        children: [
+          "/performing-tasks/",
+          "/task-state/",
+          "/cancellation/",
+          "/managing-concurrency/",
+          "/awaiting-tasks/",
+          "/handling-errors/",
+          "/testing/",
+          "/typescript-support/",
+        ],
+      },
+      {
+        title: "API overview",
+        collapsable: false,
+      },
+    ],
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# Hello VuePress
+---
+home: true
+heroText: 🚦vue-concurrency
+tagline: A library for encapsulating asynchronous operations and managing concurrency for Vue and Composition API
+actionText: Get Started →
+actionLink: /introduction/
+features:
+  - title: Encapsulating Asynchronous Operations
+    details: Task wraps an asynchronous operation and provides helpful and detailed derived state. All with first class TypeScript support.
+  - title: Cancellation
+    details: Tasks, as opposed to promises, can be cancelled and they cancel automatically if the component is unmounted.
+  - title: Managing Concurrency
+    details: Tasks can be run in parallel, set as restartable, cancel or enqueued to be performed later.
+---
+
+<!-- # Hello VuePress -->
