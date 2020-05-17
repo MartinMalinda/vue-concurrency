6f55c4351f58d169f0604b0eeb0b689e5d0782c4
martinmalinda <malindacz@gmail.com>
2020-05-17 21:20:18 +0200

    add wrap utils

---
+++ __tests__/wrap-utils.ts
@@ -0,0 +1,98 @@
+import { mockSetup } from "./task";
+import useTask from "../src/index";
+import {
+  usePipeTask,
+  useParallelTask,
+  useSequentialTask,
+} from "../src/wrap-utils";
+
+describe("usePipeTask", () => {
+  test("passes value well", async () => {
+    await mockSetup(async () => {
+      const addTask = useTask(function*(signal, a: number, b: number) {
+        return a + b;
+      });
+      const add10Task = useTask(function*(signal, a: number) {
+        return a + 10;
+      });
+      const add20Task = useTask(function*(signal, a: number) {
+        return a + 20;
+      });
+      const pipeTask = usePipeTask(addTask, add10Task, add20Task);
+      const number = await pipeTask.perform(100, 100);
+      expect(number).toBe(230);
+    });
+  });
+
+  test("works with just one task", async () => {
+    await mockSetup(async () => {
+      const addTask = useTask(function*(signal, a: number, b: number) {
+        return a + b;
+      });
+      const pipeTask = usePipeTask(addTask);
+      const number = await pipeTask.perform(100, 100);
+      expect(number).toBe(200);
+    });
+  });
+});
+
+describe("useParallelTask", () => {
+  test("passes arguments to all tasks", async () => {
+    await mockSetup(async () => {
+      const addTask = useTask(function*(signal, a: number, b: number) {
+        return a + b;
+      });
+      const add10Task = useTask(function*(signal, a: number) {
+        return a + 10;
+      });
+      const add20Task = useTask(function*(signal, a: number) {
+        return a + 20;
+      });
+      const pipeTask = useParallelTask(addTask, add10Task, add20Task);
+      const [firstResult, secondResult, thirdResult] = await pipeTask.perform(
+        100,
+        100
+      );
+      expect(firstResult).toBe(200);
+      expect(secondResult).toBe(110);
+      expect(thirdResult).toBe(120);
+    });
+  });
+
+  test("works with just one task", async () => {
+    await mockSetup(async () => {
+      const addTask = useTask(function*(signal, a: number, b: number) {
+        return a + b;
+      });
+      const pipeTask = useParallelTask(addTask);
+      const [number] = await pipeTask.perform(100, 100);
+      expect(number).toBe(200);
+    });
+  });
+});
+
+describe("useSequentialTask", () => {
+  test("passes arguments to all tasks", async () => {
+    await mockSetup(async () => {
+      const addTask = useTask(function*(signal, a: number, b: number) {
+        return a + b;
+      });
+      const add10Task = useTask(function*(signal, a: number) {
+        return a + 10;
+      });
+      const add20Task = useTask(function*(signal, a: number) {
+        return a + 20;
+      });
+      const pipeTask = useSequentialTask(addTask, add10Task, add20Task);
+      const [firstResult, secondResult, thirdResult] = await pipeTask.perform(
+        100,
+        100
+      );
+      expect(firstResult).toBe(200);
+      expect(secondResult).toBe(110);
+      expect(thirdResult).toBe(120);
+    });
+  });
+});
+
+// TODO: test cancelation cascade
---
+++ src/wrap-utils.ts
@@ -0,0 +1,40 @@
+import { Task } from "./Task";
+import useTask from "./index";
+
+export function usePipeTask<T, U extends any[]>(
+  firstTask: Task<any, U>,
+  ...restTasks: Task<any, any>[]
+): Task<T, U> {
+  return useTask(function*(signal, ...args: U) {
+    let result = yield firstTask.perform(...args).canceledOn(signal);
+    for (let task of restTasks) {
+      result = yield task.perform(result).canceledOn(signal);
+    }
+
+    return result as T;
+  });
+}
+
+export function useParallelTask(...tasks: Task<any, any>[]): Task<any[], any> {
+  return useTask(function*(signal, ...args) {
+    const instances = tasks.map((task) => {
+      return task.perform(...args).canceledOn(signal);
+    });
+
+    const values = yield Promise.all(instances);
+    return values as any[];
+  });
+}
+
+export function useSequentialTask<T, U extends any[]>(
+  ...tasks: Task<any, any>[]
+): Task<any, U> {
+  return useTask(function*(signal, ...args: U) {
+    const instances: any[] = [];
+    for (let task of tasks) {
+      instances.push((yield task.perform(...args).canceledOn(signal)) as any);
+    }
+
+    return instances;
+  });
+}
