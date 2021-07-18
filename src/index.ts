8c5171e765c267a6f93c9aca73e1b761a61237dd
martinmalinda <malindacz@gmail.com>
2021-07-18 12:12:14 +0200

    export types from root

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export { usePipeTask, useParallelTask, useSequentialTask } from "./wrap-utils";
 export { printTask, waitForValue, getCancelToken, timeout, useAsyncTask } from "./utils/general";
 export { useTaskPrefetch, useSSRPersistance } from "./utils/ssr-utils";
 
-export { default as useTaskGroup } from "./TaskGroup";
-export { default as useTask } from "./Task";
+export { default as useTaskGroup, TaskGroup } from "./TaskGroup";
+export { default as useTask, Task } from "./Task";
+export { TaskInstance } from "./TaskInstance";
+export { YieldReturn } from './types';
