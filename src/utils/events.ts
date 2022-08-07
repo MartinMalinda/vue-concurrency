b1c6e008ad2ee05b9d1c8c20f78f67da296c10ff
Tomas Scheel <tomas@scheel.site>
2022-08-07 18:45:28 -0500

    Removed ref on events

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,29 +1,29 @@
 import {ref} from "./api";
 import {IRemoveEvent, Events, IAddEvent, IFireEvent} from "../types/events"
 
-const events = ref<Events>({
+const events: Events = {
     onError: {},
-})
+}
 
 export default function globalEvents() { 
     const hasEvent = ({target, key}: IRemoveEvent) => {
-        return events.value[target][key] !== undefined;
+        return events[target][key] !== undefined;
     }
 
     const addEvent = ({target, key, handler}: IAddEvent) => {
-        events.value[target][key] = handler;
+        events[target][key] = handler;
     }
 
     const removeEvent = ({target, key}: IRemoveEvent) => {
-        events.value[target][key] = undefined;
+        events[target][key] = undefined;
     }
 
     const clearTargetEvents = ({target}: IRemoveEvent) => {
-        events.value[target] = {};
+        events[target] = {};
     }
 
     const fireEvent = ({target, eventArgs}: IFireEvent) => {
-        const eventsToFire = Object.values(events.value[target]);
+        const eventsToFire = Object.values(events[target]);
         eventsToFire.filter(x => x !== undefined).forEach(x => x!(eventArgs))
     }
 
